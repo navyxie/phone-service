81c7381c0fdabf38142b0f3ac3c63e002aae8749
Navy <hxie@kalengo.com>
2015-08-24 19:50:17 +0800

    修复tenpay插件请求出错导致程序异常退出的bug

--- plugin/tenpay.js
+++ plugin/tenpay.js
@@ -9,7 +9,7 @@ var pluginParam = {
 			if(err){
 				cb('tenpay plugin return data error, data is : '+err);
 			}else{
-				if(util.isObject(data) && util.isObject(data.root)){
+				if(util.isObject(data) && util.isObject(data.root) && (data.root.supplier && data.root.supplier.length) && (data.root.province && data.root.province.length) && (data.root.city && data.root.city.length)){
 					result[util.getSupplierKey()] = data.root.supplier[0];
 					result[util.getProvinceKey()] = data.root.province[0];
 					result[util.getCityKey()] = data.root.city[0];
