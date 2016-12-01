3253fd0e490a240c5639c4875a7019bfdd2c72e7
navyxie <navyxie2010@gmail.com>
2016-12-01 14:37:22 +0800

    修正ip138请求,不带header会返回乱码的bug

--- plugin/ip138.js
+++ plugin/ip138.js
@@ -33,6 +33,19 @@ var pluginParam = {
 	url:function(phone){
 		return 'http://www.ip138.com:8080/search.asp?action=mobile&mobile='+phone;
 	},
+	option: {
+		headers: {
+			'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+			'Accept-Encoding': 'gzip, deflate, sdch',
+			'Accept-Language': 'zh-CN,zh;q=0.8,en;q=0.6,zh-TW;q=0.4',
+			'Cache-Control': 'no-cache',
+			'Connection': 'keep-alive',
+			'Host': 'www.ip138.com:8080',
+			'Pragma': 'no-cache',
+			'Upgrade-Insecure-Requests': 1,
+			'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.108 Safari/537.36'
+		}
+	},
 	model:2
 }
 module.exports = pluginParam;
